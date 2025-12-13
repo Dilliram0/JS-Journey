5b41f3ebfd89cbd879abbf71f3145f5efeb505ef
Dilliram0 <bradjohn235@gmail.com>
2025-12-13 08:04:42 +0000

    Datatypes operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -28,7 +28,43 @@ let BooleanisLoggedIn = Boolean(isLoggedIn);
 let someNumber = true;
 
 let stringNumber = String(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
 
 // everything => string
+
+// ****************Operations**************** //
+
+let value = 3;
+let negVal = -value;
+// console.log(negVal);
+
+// console.log( 2 + 2);
+// console.log( 2 - 2);
+// console.log( 2 * 2);
+// console.log( 2 / 2);
+
+
+let str1 = "Hello";
+let str2 = " Krishna";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log ( (3 + 4) * 5 / 7);
+
+
+// console.log(true);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter = 100;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
