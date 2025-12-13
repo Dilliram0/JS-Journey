70032dbea5a16f9c8c51f5613499bdf06b563f50
Dilliram0 <bradjohn235@gmail.com>
2025-12-13 06:59:06 +0000

    Datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treats all Js code as newer JS version
+
+// alert ("Hello") // we are using nodejs, not browser
+
+console.log (3
+    + 3)  // code readability should be high
+
+console.log("Abisek");
+
+let name = "Abishek";
+let age = 17;
+let isLoggedIn = false;
+
+
+
+// DATA TYPES //
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value / empty space
+// undefined =>
+// symbol => uniqueness
+
+
+// Object
+
+console.table([typeof age,typeof name,typeof isLoggedIn]);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //Object
+
