be14d089aac9cbb256780319ef3f5c0bdcad5f34
Dilliram0 <bradjohn235@gmail.com>
2025-12-13 07:34:30 +0000

    conversion

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = "abi";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => 33
+// 33abc => NaN
+// boolean => 0/1
+// null => 0
+// undefined => NaN
+
+
+let isLoggedIn = Object;
+
+let BooleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof BooleanisLoggedIn);
+// console.log(BooleanisLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Abisek" => true
+// null & undefined => false
+
+let someNumber = true;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// everything => string
