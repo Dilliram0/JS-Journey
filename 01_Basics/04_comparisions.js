bebbe188ac765dfc4c4f60ed2275776a30f7613a
Dilliram0 <bradjohn235@gmail.com>
2025-12-14 08:17:42 +0000

    comparision

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,20 @@
+// console.log(2>1)
+// console.log(2=1)
+// console.log(2 >= 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log( null > 0);
+// console.log( null == 0);// => checks the values
+// console.log( null === 0);// => checks the datatypes
+// console.log( null >= 0); //=>true because > converts null to 0 //
+
+// console.log( undefined > 0);
+// console.log(undefined >= 0);
+// console.log ( undefined == null);//=> true
+// console.log( undefined === null);// => false
+
+console.log("2" == 2); // => true
+console.log("2" === 2); // => false
