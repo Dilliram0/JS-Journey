8795551c63053685d2a54a0d5ec6f55f1b4a7b42
Dilliram0 <bradjohn235@gmail.com>
2025-12-19 10:14:43 +0000

    scopes in depth

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,60 @@ if(true){
     let a = 10;
     const b = 20;
     var c = 30;
-    console.log("Inner a:",a)
+    // console.log("Inner a:",a)
 }
 
 // console.log(a); // undefined
 // console.log(b); // undefined
-console.log(c); // 30
+// console.log(c); // 30
 
 
-// Global scope
+
+
+function one(){
+    const username = "abhisek";
+
+    function two(){
+        const website = "youtube"
+        // console.log(username); // works it can take value from upper scope
+    }
+
+    // console.log(website); // doesnt work it cant take value from block scope
+
+    two()
+
+}
+
+one()
+
+
+if (true) {     
+    const username = "Krishna";
+    if(username === "Krishna"){
+        const website = "youtube";
+        // console.log(website+username);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username)
+
+
+// ++++++++++ Interesting ++++++++++++++ //
+
+
+
+console.log(addOne(5)); // can access before initialization 
+
+function addOne(num){
+    return num + 1;
+}
+
+// addTwo(5); // Cannot access before initialization
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+
