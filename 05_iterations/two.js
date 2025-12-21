3a2c5d90f2b98b69c4388695df29ddc90ac6ae31
Dilliram0 <bradjohn235@gmail.com>
2025-12-21 08:40:29 +0000

    do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,32 @@
+// while loop
+
+let index = 0;
+while(index <= 10){
+    // console.log(`value of muffin is ${index}`);
+    index = index +2;
+}
+
+let arr = 0;
+let myArray = ["flash","batman","ironman"];
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+    arr = arr + 1;
+}
+
+
+// do while loop
+
+// let score = 0;
+
+// do {
+//     console.log(score);
+//     score ++;
+// } while (score <= 10);
+
+let score = 11;
+
+do {
+    console.log(score); // => 11
+    score ++;
+} while (score <= 10);
